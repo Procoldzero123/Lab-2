6333ae689ab2f4d9dfc3edcab19af018f812b085
BevanTan <BEVANTAN.24@ICHAT.SP.EDU.SG>
2025-05-06 15:31:12 +0800

    added changes to return values

--- bmi.py
+++ bmi.py
@@ -1,24 +1,29 @@
 def calculate_bmi(height, weight):
-    print("height " +str(height))
-    print("weight " +str(weight))
-    bmi = weight/(height**2)
-    print("bmi "+str(bmi) )
-    return bmi
+    print("height " + str(height))
+    print("weight " + str(weight))
+    bmi = weight / (height ** 2)
+    print("bmi " + str(bmi))
 
-def classify_bmi(bmi):
-    if(bmi<18.5):
+    # Classify and return value
+    if bmi < 18.5:
+        return -1  # Underweight
+    elif 18.5 <= bmi <= 25:
+        return 0   # Normal weight
+    else:
+        return 1   # Overweight
+
+def classify_bmi(bmi_class):
+    if bmi_class == -1:
         print("Underweight")
-    elif(bmi>=18.5 and bmi<=25):
+    elif bmi_class == 0:
         print("Normal Weight")
-    else:   
-        print("Overweight,better exercise!")
+    elif bmi_class == 1:
+        print("Overweight, better exercise!")
     return
-
 
 def main():
-    output = calculate_bmi(1.7,60)
+    output = calculate_bmi(1.7, 90)
     classify_bmi(output)
-    return
 
 if __name__ == "__main__":
     main()
